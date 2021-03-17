c3cd93eb31f8f4a6b70cffeb8106c18075d45815
manik2255 <80624541+manik2255@users.noreply.github.com>
2021-03-17 22:22:51 +0530

    Update ASSIGNMENT_2_GRAPH.py

--- ASSIGNMENT_2/ASSIGNMENT_2_GRAPH.py
+++ ASSIGNMENT_2/ASSIGNMENT_2_GRAPH.py
@@ -1,12 +1,26 @@
 import numpy as np
 import matplotlib.pyplot as plt
+from scipy.stats import expon
+import seaborn as sns
+sns.set(color_codes=True)
+sns.set(rc={'figure.figsize':(6,6)})
 
-x = np.linspace(0, 100, 100)
+x = np.linspace(0, 100, 101)
 f =  np.exp(-x)
-plt.title("probability density function")
 plt.plot(x, f, linewidth=2, label=r"$f(x) = e^{-x}$")
 plt.xlim(0, 100)
 plt.ylabel("f(x)") 
 plt.xlabel("x")
 plt.legend(fontsize=14)
 plt.show()
+
+
+
+data_expon = expon.rvs(scale=1,loc=0,size=1000)
+plt.title("probability density function")
+ax = sns.distplot(data_expon,
+                  kde=True,
+                  bins=100,
+                  color='blue',
+                  hist_kws={"linewidth": 15,'alpha':1})
+ax.set(xlabel='Exponential Distribution', ylabel='Frequency')
