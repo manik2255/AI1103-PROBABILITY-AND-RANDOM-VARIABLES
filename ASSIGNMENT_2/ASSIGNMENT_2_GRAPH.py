c64d76fb672a0279a7b2ee7329b7f0fc543f5353
manik2255 <80624541+manik2255@users.noreply.github.com>
2021-03-18 23:38:07 +0530

    Update ASSIGNMENT_2_GRAPH.py

--- ASSIGNMENT_2/ASSIGNMENT_2_GRAPH.py
+++ ASSIGNMENT_2/ASSIGNMENT_2_GRAPH.py
@@ -1,26 +1,47 @@
+#Importing numpy, scipy, mpmath and pyplot
 import numpy as np
+import scipy 
 import matplotlib.pyplot as plt
-from scipy.stats import expon
-import seaborn as sns
-sns.set(color_codes=True)
-sns.set(rc={'figure.figsize':(6,6)})
 
 x = np.linspace(0, 100, 101)
 f =  np.exp(-x)
-plt.plot(x, f, linewidth=2, label=r"$f(x) = e^{-x}$")
+plt.plot(x, f, linewidth=1, label=r"$f(x) = e^{-x}$")
 plt.xlim(0, 100)
 plt.ylabel("f(x)") 
 plt.xlabel("x")
 plt.legend(fontsize=14)
 plt.show()
 
+maxrange=50
+maxlim=100.0
+x = np.linspace(0,maxlim,maxrange)#points on the x axis
+simlen = int(1e6) #number of samples
+err = [] #declaring probability list
+pdf = [] #declaring pdf list
 
+#randvar = np.random.normal(0,1,simlen)
+#randvar = np.loadtxt('uni.dat',dtype='double')
+randvar = np.loadtxt('gau.dat',dtype='double')
 
-data_expon = expon.rvs(scale=1,loc=0,size=1000)
+for i in range(0,maxrange):
+	err_ind = np.nonzero(randvar < x[i]) #checking probability condition
+	err_n = np.size(err_ind) #computing the probability
+	err.append(err_n/simlen) #storing the probability values in a list
+
+	
+for i in range(0,maxrange-1):
+	test = (err[i+1]-err[i])/(x[i+1]-x[i])
+	pdf.append(test) #storing the pdf values in a list
+
+def gauss_pdf(x):
+	return (-1)*np.exp(-x)
+	
+vec_gauss_pdf = scipy.vectorize(gauss_pdf)
+
+plt.plot(x[0:(maxrange-1)].T,pdf,'o')
+plt.plot(x,vec_gauss_pdf(x))#plotting the CDF
+plt.grid() #creating the grid
 plt.title("probability density function")
-ax = sns.distplot(data_expon,
-                  kde=True,
-                  bins=100,
-                  color='blue',
-                  hist_kws={"linewidth": 15,'alpha':1})
-ax.set(xlabel='Exponential Distribution', ylabel='Frequency')
+plt.xlabel('$x_i$')
+plt.ylabel('$p_X(x_i)$')
+plt.legend(["Numerical","Theory"])
