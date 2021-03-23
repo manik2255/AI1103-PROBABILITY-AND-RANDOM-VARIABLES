082770e3ba05abe152d052263debd15051047fc6
manik2255 <80624541+manik2255@users.noreply.github.com>
2021-03-23 17:30:26 +0530

    Add files via upload

---
+++ ASSIGNMENT_3/assign_3.py
@@ -0,0 +1,30 @@
+from scipy.stats import geom
+import matplotlib.pyplot as plt
+
+X = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
+p = 0.5
+
+# Calculate geometric probability distribution
+geom_pd = geom.pmf(X, p)
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+ax.plot(X, geom_pd, label='geom pmf')
+plt.ylabel("P(x)", fontsize="10")
+plt.xlabel("X", fontsize="10")
+ax.vlines(X, 0, geom_pd, colors='b', lw=5, alpha=0.5)
+
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+ax.plot(X, geom_pd, label='geom pmf')
+plt.ylabel("P(y)", fontsize="10")
+plt.xlabel("Y", fontsize="10")
+ax.vlines(X, 0, geom_pd, colors='b', lw=5, alpha=0.5)
+
+count = 0
+for i in range(10000):
+  X = geom.rvs(0.5)
+  Y = geom.rvs(0.5)
+  if 2*Y <= X:
+        count += 1
+        
+print("Experimental probability = ", count / 10000)
+print("which is approximately equal to 0.2857(theoritical probability)")
