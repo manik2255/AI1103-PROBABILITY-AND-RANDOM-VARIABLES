e01c5c756f4ad6db178d5f6cadff0496e0e88e71
manik2255 <80624541+manik2255@users.noreply.github.com>
2021-03-24 14:49:45 +0530

    Update assign_3.py

--- ASSIGNMENT_3/assign_3.py
+++ ASSIGNMENT_3/assign_3.py
@@ -1,4 +1,5 @@
 from scipy.stats import geom
+import numpy as np
 import matplotlib.pyplot as plt
 
 X = [1, 2, 3, 4, 5, 6, 7, 8, 9, 10]
@@ -6,19 +7,29 @@ p = 0.5
 
 # Calculate geometric probability distribution
 geom_pd = geom.pmf(X, p)
+# Calculate cdf
+geom_cd = geom.cdf(X, p)
 
 fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.plot(X, geom_pd, label='geom pmf')
 plt.ylabel("P(x)", fontsize="10")
 plt.xlabel("X", fontsize="10")
 ax.vlines(X, 0, geom_pd, colors='b', lw=5, alpha=0.5)
+plt.show()
 
 fig, ax = plt.subplots(1, 1, figsize=(8, 8))
 ax.plot(X, geom_pd, label='geom pmf')
 plt.ylabel("P(y)", fontsize="10")
 plt.xlabel("Y", fontsize="10")
 ax.vlines(X, 0, geom_pd, colors='b', lw=5, alpha=0.5)
+plt.show()
 
+fig, ax = plt.subplots(1, 1, figsize=(8, 8))
+ax.plot(X, geom_cd, label='geom cdf')
+plt.ylabel("F_Y(y)", fontsize="10")
+plt.xlabel("Y", fontsize="10")
+ax.vlines(X, 0, geom_cd, colors='b', lw=5, alpha=0.5)
+plt.show()
 count = 0
 for i in range(10000):
   X = geom.rvs(0.5)
