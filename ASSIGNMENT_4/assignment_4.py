ebc89079b83d053a4c0ae49a700fdee1fae14fd5
manik2255 <80624541+manik2255@users.noreply.github.com>
2021-03-27 15:45:24 +0530

    Add files via upload

---
+++ ASSIGNMENT_4/assignment_4.py
@@ -0,0 +1,30 @@
+import numpy as np
+# import uniform distribution
+from scipy.stats import uniform
+from matplotlib import pyplot as plt
+# data to be plotted 
+x = np.arange(0,2)  
+y =np.exp(-x)
+
+
+def ecdf(a):
+    x, counts = np.unique(a, return_counts=True)
+    cusum = np.cumsum(counts)
+    return x, cusum / cusum[-1]
+    return x
+
+U = uniform.rvs(size=1000)
+V =-np.log(U)
+x = ecdf(V)[0]
+y = ecdf(V)[1]
+x = np.insert(x, 0, x[0])
+y = np.insert(y, 0, 0.)
+
+plt.plot(x, y,'o',label='cdf')
+plt.grid(True)
+v=np.linspace(0,15,1000)
+plt.plot(v,np.exp(-v),label='pdf')
+plt.xlabel("$x$")
+plt.ylabel("$F_x(x)$")
+plt.show()
+
